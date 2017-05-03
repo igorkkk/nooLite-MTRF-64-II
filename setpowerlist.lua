bc84a680d3f4bd8ead15b21c07c49c6395c06973
Igor Kandaurov <igorkandaurov@gmail.com>
2017-05-03 18:46:12 +0300

    ====

--- setpowerlist.lua
+++ setpowerlist.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 do
 local cell = cell or ""
 local modcell = modcell or "0"
@@ -42,43 +41,3 @@ if file.open("powers.lua", "r") then
 end
 
 end
-=======
-do
-local cell = cell or ""
-local modcell = modcell or "0"
-local remove = remove or "88"
-local ds = {}
-if cell ~= ""  then
-    ds[cell] = modcell
-end
-if file.open("powerlist.lua", "r") then
-    local s = file.read('\n')
-    while s do
-        a, b = string.match(s, "(%d+),(%d+)")
-        ds[a] = b
-        s = file.read('\n')
-    end
-    file.close()
-end 
-ds[remove] = nil
-if file.open("powers.lua", "w") then
-    for k, v in pairs(ds) do
-        local ss = ""..k..","..v
-        file.writeline(ss)
-    end
-    file.close()
-end
-
-if file.open("powers.lua", "r") then
-    local s = file.read('\n')
-  while s do
-    a, b = string.match(s, "(%d+),(%d+)")
-    print(a, b)
-    s = file.read('\n')
-end
-file.close()
-file.remove("powerlist.lua")
-file.rename("powers.lua","powerlist.lua")
-end
-end
->>>>>>> master
