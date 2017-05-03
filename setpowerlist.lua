4fa89d0a1d0bbb8569654805ccc032ecb5824e08
Igor Kandaurov <igorkandaurov@gmail.com>
2017-05-03 18:41:29 +0300

    -----

--- setpowerlist.lua
+++ setpowerlist.lua
@@ -1,4 +1,3 @@
-<<<<<<< Temporary merge branch 1
 do
 local cell = cell or ""
 local modcell = modcell or "0"
@@ -35,47 +34,5 @@ end
 file.close()
 file.remove("powerlist.lua")
 file.rename("powers.lua","powerlist.lua")
-end
-=======
-do
-local cell = "11"
-local modcell = "0"
-local remove = "88"
-local ds = {}
-
-ds[cell] = modcell
-ds["3"] = "2"
-
-local ex = "65"
-if file.open("powerlist.lua", "r") then
-    local s = file.read('\n')
-    while s do
-        a, b = string.match(s, "(%d+),(%d+)")
-        if a ~= ex then ds[a] = b end
-        s = file.read('\n')
-    end
-    file.close()
-end 
-ds[remove] = nil
-if file.open("powers.lua", "w") then
-    for k, v in pairs(ds) do
-        local ss = ""..k..","..v
-        file.writeline(ss)
-    end
-    file.close()
-end
-
-if file.open("powers.lua", "r") then
-    local s = file.read('\n')
-  while s do
-    a, b = string.match(s, "(%d+),(%d+)")
-    print(a, b)
-    s = file.read('\n')
-end
-file.close()
-file.remove("powerlist.lua")
-file.rename("powers.lua","powerlist.lua")
-end
->>>>>>> Temporary merge branch 2
 end
 end
