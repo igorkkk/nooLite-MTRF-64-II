f09230a7b260559ed86a812a229fe1b54468abc5
Igor Kandaurov <igorkandaurov@gmail.com>
2017-05-08 21:38:53 +0300

    Управляем яркостью
    Первые прикидки по управлению яркостью

--- analizebrgt.lua
+++ analizebrgt.lua
@@ -4,20 +4,18 @@
 5 - изменять яркость
 10- стоп изменениям яркости 
 --]]
-
 do
-local gotRAW = {1,2,3,4,29,5,7,8,9,}
+local gotRAW = {1,2,3,4,9,3,7,8,9,}
 local itmState = {
     {11},
-    {9,0,15,0}
+    {9,0,15,true},
+	{8,50,50,true}
 } 
 
 itmn = gotRAW[5]
+print("get cell "..itmn)
 func = ""
-
-local flaggg = false
 local cellNo
-
 for k, v in pairs(itmState) do
     if v[1] == itmn then
         cellNo = k
@@ -26,34 +24,53 @@ end
 
 if not cellNo and itmn <11 then
     cellNo = #gotRAW+1
-    itmState[cellNo] = {itmn, 0, 100, 0}
+    itmState[cellNo] = {itmn, 0, 50, true}
 end
-
-
+--
+stopbr = false -- global var
+local setbrgt = function(ceNo, cell, startbrgt, direction)
+	local brgtnow = startbrgt or 50
+	local incr = direction and 5 or -5
+	itmState[ceNo][4] = direction
+	local sendcom = function(br)
+		print( "cell: "..cell.." set at "..br)
+		itmState[ceNo][2] = br
+		itmState[ceNo][3] = br
+		comm = br
+		func = "brgt"
+		item = ""..cell
+		-- newdeal()
+	end
+	return function(call)
+		tmr.create():alarm(1000, 1, function(t) 
+			brgtnow = brgtnow + incr
+			if brgtnow > 100 or brgtnow < 0 or stopbr then 
+				tmr.stop(t)
+				tmr.unregister(t) 
+				if call then 
+					brgtnow, incr, sendcom = nil, nil, nil 
+					call() 
+				end
+			else
+				sendcom(brgtnow)
+			end
+		end)
+	end
+end
+--]]
 local z = function()
-    
     local state
     if itmn > 10 then
-        flaggg  = true
-
         func = itmn > 20 and "comf" or "comm"
         print("func = "..func)
-
-        if gotRAW[6] == 1 then
-            comm = "OFF" 
-            print("Switch OFF NOW!")
-            if cellNo then
-                itmState[cellNo] = nil
-            end
-            
-        elseif gotRAW[6] == 1 then
-            if not cellNo then
-                table.insert(itmState,{itmn})
-            end
+        if gotRAW[6] == 1 and cellNo >= 0 then
+			itmState[cellNo] = nil
+			comm = "OFF" 
+			print("Switch OFF NOW!")
+		elseif gotRAW[6] == 3 and not cellNo then
+            table.insert(itmState,{itmn})
             print("Switch ON, just NOW!")
             comm = "ON"
-        end
-
         elseif gotRAW[6] == 5 then
             if not cellNo then
                 table.insert(itmState,{itmn})
@@ -65,52 +82,28 @@ local z = function()
                 print("Switch OFF NOW!")
             end
         end
-    
-    -- for blocks that run brightness
-    
-    else
-        func = "comm"
-        print("func = "..func)
-        for k, v in pairs(itmState) do
-            if v[1] == itmn then
-                state = true
-                flaggg = true
-                if v[2] ~= v[3] and v[3] > 4 and gotRAW[6] == 2 then
-                    v[2] = v[3]
-                    print("Cell "..itmn.." Switched ON at "..v[2])
-                    comm = "ON"
-
-                elseif v[3] < 5 and gotRAW[6] == 2 then 
-                    v[2] = 100
-                    v[3] = 100
-                    print("Cell "..itmn.." Switched ON at "..v[2])
-                    comm = "ON"
-                elseif v[2] == v[3] and gotRAW[6] == 0  then
-                    v[2] = 0
-                    print("Cell "..itmn.." Switched OFF ")
-                    comm = "OFF"
-                else
-                    flaggg = false
-                end
-                -- break
-            end
-        end
-        if not state then
-            print("Cell "..itmn.." is new, made record")
-            if gotRAW[6] == 2 then 
-                table.insert(itmState,{itmn,100,100})
-                comm = "ON"
-            else
-                table.insert(itmState,{itmn,0,100})
-                comm = "OFF"
-            end
-            flaggg = true
-        end 
+        print(itmn, func, comm)
+		-- newdeal()
+	else
+		if gotRAW[6] == 10 then 
+			stopbr = true 
+		else
+			local wr
+			local r = function()
+				wr = nil
+			end
+			if gotRAW[6] == 1 then
+				wr = setbrgt(cellNo, itmn, itmState[cellNo][2], false)
+			elseif gotRAW[6] == 3 then
+				wr = setbrgt(cellNo, itmn, itmState[cellNo][2], true)
+			elseif gotRAW[6] == 5 then
+				local dir = not itmState[cellNo][4]
+				wr = setbrgt(cellNo, itmn, itmState[cellNo][2], dir)
+			end
+		wr(r)
+		end
+	end 
    --]]
-    end
-    print(itmn, func, comm, flaggg)
--- if flaggg then newdeal() end
 end
-
 z()
 end
